ee7f58ce8a12e97ef91bb68755a2d24bc1067655
otimthomas <totim444@hotmail.co.uk>
2020-05-15 05:39:24 +0300

    Hero, Home, and NotFound Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import {ProductContext} from "../context/ProductContext";
+import Hero from "../components/Hero";
+import {Link} from "react-router-dom";
 
 export default function HomePage() {
   console.log(React.useContext(ProductContext));
   return (
     <>
-      <h1>Hello from Home page</h1>
+      <Hero title='awesome gadgets' max='true'>
+        <Link to='/products' className="main-link m-4">our products</Link>
+      </Hero>
     </>
   );
 }
